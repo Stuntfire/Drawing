54d23c1e817848a64f8cf900de3d04afed7b9ea1
Søren Roed Schack <soer622t@edu.easj.dk>
2016-11-13 15:06:16 +0100

    Added a new class: Circle.cs

---
+++ Drawing/Circle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Shapes;
+using Windows.UI.Xaml.Controls;
+
+namespace Drawing
+{
+    class Circle : IDraw, IColor
+    {
+        private int diameter;
+        private int locX = 0, locY = 0;
+        private Ellipse circle = null;
+    }
+}
