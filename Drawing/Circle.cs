7f3555503a7720cfe55d220d28701a62b53b9310
Søren Roed Schack <soer622t@edu.easj.dk>
2016-11-13 15:34:02 +0100

    hmm, added a lot I didn't understand? 2 x click Tapped in proberties in DrawingPad.xaml file and a new xaml.cs file was auto-generated and added some if statements to that file.

--- Drawing/Circle.cs
+++ Drawing/Circle.cs
@@ -20,5 +20,39 @@ namespace Drawing
         {
             this.diameter = diameter;
         }
+
+        void IDraw.SetLocation(int xCoord, int yCoord)
+        {
+            this.locX = xCoord;
+            this.locY = yCoord;
+        }
+
+        void IDraw.Draw(Canvas canvas)
+        {
+            if (this.circle != null)
+            {
+                canvas.Children.Remove(this.circle);
+            }
+            else
+            {
+                this.circle = new Ellipse();
+            }
+
+            this.circle.Height = this.diameter;
+            this.circle.Width = this.diameter;
+            Canvas.SetTop(this.circle, this.locY);
+            Canvas.SetLeft(this.circle, this.locX);
+            canvas.Children.Add(this.circle);
+        }
+
+        void IColor.SetColor(Color color)
+        {
+            if (this.circle != null)
+            {
+                SolidColorBrush brush = new SolidColorBrush(color);
+                this.circle.Fill = brush;
+
+            }
+        }
     }
 }
