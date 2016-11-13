2716f19837fd3e17271e5015d3d71bb0984c479f
Søren Roed Schack <soer622t@edu.easj.dk>
2016-11-13 14:39:52 +0100

    Added a class (Square.cs) and 3 instance fields.

---
+++ Drawing/Square.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Shapes;
+using Windows.UI.Xaml.Controls;
+
+
+namespace Drawing
+{
+    class Square : IDraw, IColor
+    {
+        private int sideLength;
+        private int locX = 0, locY = 0;
+        private Rectangle rect = null;
+    }
+}
